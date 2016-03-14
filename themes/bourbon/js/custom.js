1a72cf1bda593858d9339e0a772112b9fe6e9c06
Jacob Moen <jacmoe.dk@gmail.com>
2016-03-14 20:08:05 +0100

    Cookie law script

--- themes/bourbon/js/custom.js
+++ themes/bourbon/js/custom.js
@@ -1,2 +1,12 @@
 $(document).ready(function() {
+    $.cookiesDirective({
+      backgroundOpacity: '85',
+      backgroundColor: '#262626',
+      linkColor: '#FFF',
+      buttonClass: 'button small',
+      position: 'bottom',
+      explicitConsent: false,
+      message: 'Dette netsted bruger cookies til statistik og brugerkommentarer. Du acepterer brugen af disse cookies om du vælger at fortsætte med at besøge netstedet.',
+      privacyPolicyUri: '/cookies'
+    });
 });
